58660ec527c54ec4e4247bdd4064b3b212f4ad59
mateocossio17 <mcossio@ucb.edu.bo>
2025-04-27 21:47:03 -0400

    Add files via upload

---
+++ ejercicios_de_clase.py
@@ -0,0 +1,150 @@
+#ejercicio 1
+edad = 17
+
+if edad >= 18:
+  print("Puedes votar")
+else:
+  print("No puedes votar")
+#ejercicio2 
+numero1 = 8
+numero2 =5 
+
+if numero1 > numero2:
+  print(f"El número mayor es {numero1}")
+elif numero2 > numero1:
+  print(f"El número mayor es {numero2}")
+else:
+  print("Ambos números son iguales")
+
+
+# 3 
+muestra = 81
+if muestra <80 and muestra >95:
+  print("calidad aceptable")
+
+  
+
+else: print ("calidad excelente ")
+   
+#ejercicio 
+peso = 18  
+if peso >= 5 : 
+  print("ligero")
+elif peso < 5 and peso >20: 
+  print("medio")
+elif peso >20 :
+  print("pesado") 
+
+#ejercicio 5 
+anios = 8
+salario = 1000
+if anios < 5 and anios >= 10  :
+  print(salario * 0.10)
+elif anios > 10 : 
+  print(salario * 0.20)
+# ejercicio 6
+numero_1 = 15
+numero_2 = 30 
+if(numero_1 and numero2 %3==0) and (numero_1 and numero2 %5==0):
+  print("si son cosos")
+ 
+
+
+  
+
+  
+  
+
+#formulas de ingenieria 
+x= 2 
+y= 2
+z= (x*x + y *y/ y + x )
+print(z)
+# identificador de triangulos 
+lado_1 = 5
+lado_2 = 10 
+lado_3 = 5
+if lado_1 == lado_2 and lado_2 == lado_3 :
+  print ("felicidades , tu triandulo es equilatero")
+elif lado_1 != lado_2 and lado_2 == lado_3 :
+  print('felicidades tienes un triangulo isoceles ')
+elif lado_1 == lado_3 and lado_2 != lado_1 : 
+  print ('felicidades es isoceles ')
+elif lado_1 != lado_2 and lado_2 != lado_3 and lado_1 != lado_3 :
+  print ("es un triangulo escaleno")
+
+#comprobador de año bisiesto 
+año = 2100
+if (año % 4 == 0 and año %100 != 0 ) or año % 400 == 0 :
+    print('el  es bisiesto')
+else:
+  print("el año no es bisciesto bb")
+
+
+#calculo costos de envio 
+costo_producto= 100
+peso_envio = 3
+origen_del_pedido = 1 #se coloca 1 para pedidos nacionales o 2 para internacionales 
+if peso_envio <= 5 :
+  costo_envio= 20 
+  costo_total= costo_envio + costo_producto
+  if origen_del_pedido == 1 :
+    costo_descontado = costo_total - (costo_total * 0.10)
+    print  ("tu costo es de ", costo_descontado)
+  else:
+    print("tu costo es de ", costo_total)
+
+elif peso_envio>5 and peso_envio<20:
+  costo_envio= 30
+  costo_total= costo_envio + costo_producto
+  if origen_del_pedido == 1 :
+    costo_descontado = costo_total - (costo_total * 0.10)
+    print  ("tu costo es de ", costo_descontado)
+  else:
+    print("tu costo es de ", costo_total)
+elif peso_envio== 20 :
+  costo_envio= 50
+  costo_total= costo_envio + costo_producto
+  if origen_del_pedido == 1 :
+    costo_descontado = costo_total - (costo_total * 0.10)
+    print  ("tu costo es de ", costo_descontado)
+  else:
+    print("tu costo es de ", costo_total)
+
+else:
+  print("solo aceptamos hata 20 kg ")
+
+# digito mas grande de un numero no entendi 
+numero= 456322189
+
+num1 =str(numero)
+mayor = 0
+for letra in num1:
+  if (int(letra) > mayor):
+     mayor = int(letra) 
+print("el dígito mayor es:",str(mayor))
+ 
+  
+  
+  
+
+
+
+# comparacion de promedios 
+
+
+
+  
+
+
+
+
+    
+    
+  
+
+
+
+
+
+  
